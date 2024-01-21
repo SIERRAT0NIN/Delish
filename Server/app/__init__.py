fb45a4bf2bac7e439c45df4c7e73cd07f521833f
dader34 <56900960+dader34@users.noreply.github.com>
2024-01-21 03:58:36 -0800

    Add messages route

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import sqlalchemy
 
 # Assuming .models is a module in the same package
-from .models import db, User  # Import your models here
+from .models import db, User, Message  # Import your models here
 from flask_cors import CORS
 from flask_migrate import Migrate
 
@@ -12,7 +12,7 @@ from flask_migrate import Migrate
 def create_app():
     app = Flask(__name__)
     # Configure your app
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DATABASE.db'
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///DATABASE.db"
 
     db.init_app(app)
     Migrate(app, db)
@@ -20,61 +20,100 @@ def create_app():
     # Set up CORS to allow all origins for the signup route
     CORS(app, resources={r"/signup": {"origins": "*"}, r"/login": {"origins": "*"}})
 
-
     # Set up Flask-RESTful
     api = Api(app)
 
     # Define your RESTful routes
     class SignUp(Resource):
         def post(self):
-            data = request.get_json()   
+            data = request.get_json()
 
-            username = data.get('username')
-            email = data.get('email')
-            password = data.get('password')
+            username = data.get("username")
+            email = data.get("email")
+            password = data.get("password")
 
             if not all([username, email, password]):
-                return {'message': 'Missing data'}, 400
+                return {"message": "Missing data"}, 400
 
-            existing_user = User.query.filter((User.username == username) | (User.email == email)).first()
+            existing_user = User.query.filter(
+                (User.username == username) | (User.email == email)
+            ).first()
             if existing_user:
-                return {'message': 'Username or email already taken'}, 400
+                return {"message": "Username or email already taken"}, 400
 
             hashed_password = generate_password_hash(password)
-            new_user = User(username=username, email=email, password_hash=hashed_password)
+            new_user = User(
+                username=username, email=email, password_hash=hashed_password
+            )
 
             try:
                 db.session.add(new_user)
                 db.session.commit()
-                return {'message': 'User created successfully'}, 201
+                return {"message": "User created successfully"}, 201
             except sqlalchemy.exc.IntegrityError as e:
                 db.session.rollback()
-                return {'error': str(e)}, 500
+                return {"error": str(e)}, 500
 
     # Add the SignUp resource to the API
-    api.add_resource(SignUp, '/signup')
+    api.add_resource(SignUp, "/signup")
+
     class Login(Resource):
         def post(self):
             data = request.get_json()
 
-            email = data.get('email')
-            password = data.get('password')
+            email = data.get("email")
+            password = data.get("password")
 
             if not email or not password:
-                return {'message': 'Email and password are required'}, 400
+                return {"message": "Email and password are required"}, 400
 
             user = User.query.filter_by(email=email).first()
 
             if user and check_password_hash(user.password_hash, password):
                 # User authenticated successfully
                 # Here you would typically generate a token or session
-                return {'message': 'Login successful'}, 200
+                return {"message": "Login successful"}, 200
             else:
                 # Authentication failed
-                return {'message': 'Invalid username or password'}, 401
+                return {"message": "Invalid username or password"}, 401
 
     # Add the Login resource to the API
-    api.add_resource(Login, '/login')
+    api.add_resource(Login, "/login")
+
+    class Messages(Resource):
+        def get(self):
+            return [
+                message.to_dict(only=("id", "content"))
+                for message in Message.query.all()
+            ]
+
+        def post(self):
+            json = request.json
+
+            sender_id = json.get("sender_id")
+
+            reciever_id = json.get("reciever_id")
+
+            chat_id = json.get("chat_id")
+
+            content = json.get("content")
+
+            try:
+                message = Message(
+                    sender_id=sender_id,
+                    reciever_id=reciever_id,
+                    chat_id=chat_id,
+                    content=content,
+                )
+                db.session.add(message)
+                db.session.commit()
+                return message.to_dict(only=('id'))
+
+            except Exception as e:
+                db.session.rollback()
+                return {"error": e.args}
+            
+    api.add_resource(Messages,'/messages')
 
     return app
 
--- Server/app/models.py
+++ Server/app/models.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.sql import func
 
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
-class Profile(db.Model):
+class Profile(db.Model,SerializerMixin):
     __tablename__ = 'profiles'
 
     __table_args__ = {'extend_existing': True}
@@ -18,7 +19,7 @@ class Profile(db.Model):
     def __repr__(self):
         return f'<Profile {self.id}>'
     
-class Comment(db.Model):
+class Comment(db.Model,SerializerMixin):
     __tablename__ = 'comments'
     __table_args__ = {'extend_existing': True}
 
@@ -36,7 +37,7 @@ db.Column('post_id', db.Integer, db.ForeignKey('posts.id'), primary_key=True),
 db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True),
 extend_existing=True)
 
-class Post(db.Model):
+class Post(db.Model,SerializerMixin):
     __tablename__ = 'posts'
     __table_args__ = {'extend_existing': True}
 
@@ -56,7 +57,7 @@ class Post(db.Model):
 
 
 
-class Tag(db.Model):
+class Tag(db.Model,SerializerMixin):
     __tablename__ = 'tags'
     __table_args__ = {'extend_existing': True}
 
@@ -67,7 +68,7 @@ class Tag(db.Model):
     def __repr__(self):
         return f'<Tag {self.name}>'
 
-class User(db.Model):
+class User(db.Model,SerializerMixin):
     __tablename__ = 'users'
     __table_args__ = {'extend_existing': True}
 
@@ -77,9 +78,53 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     comments = db.relationship('Comment', backref='commenter', lazy='dynamic')
-
-
     profile = db.relationship('Profile', back_populates='user', uselist=False)
+
+    sent_messages = db.relationship('Message',back_populates='sender')
+
+    recieved_messages = db.relationship('Message',back_populates='reciever')
+
+    chats = db.relationship('Chat', back_populates='users')
+
+
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+
+class Message(db.Model,SerializerMixin):
+    __tablename__ = 'messages'
+
+    id = db.Column(db.Integer,primary_key=True)
+
+    sender_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
+
+    receiver_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
+
+    chat_id = db.Column(db.Integer, db.ForeignKey('chats.id'),nullable=False)
+
+    content = db.Column(db.String, nullable=False)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
+
+    sender = db.relationship('User',back_populates='sent_messages')
+
+    reciever = db.relationship('User',back_populates='recieved_messages')
+
+    chat = db.relationship('Chat',back_populates='messages')
+
+
+class Chat(db.Model,SerializerMixin):
+    __tablename__ = 'chats'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user1_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user2_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    messages = db.relationship('Message',back_populates='chat')
+
+    users = db.relationship('User', back_populates='chats')
+
+    
